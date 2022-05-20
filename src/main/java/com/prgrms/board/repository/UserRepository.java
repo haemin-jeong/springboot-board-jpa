cb116a70680ccc5f0527b859cd07b37e5ed3ffc3
jeonghaemin <jhm0828@gmail.com>
2022-05-20 16:53:31 +0900

    Feat: Repository 구현

---
+++ src/main/java/com/prgrms/board/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.prgrms.board.repository;
+
+import com.prgrms.board.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
