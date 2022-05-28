a3e2afcecf787759f3082bebe45cf31b9a3db010
jeonghaemin <jhm0828@gmail.com>
2022-05-28 23:31:23 +0900

    Refactor: fetch join 사용 시에 JPQL을 직접 작성 -> @EntityGraph 사용 방식으로 변경

--- src/main/java/com/prgrms/board/repository/PostRepository.java
+++ src/main/java/com/prgrms/board/repository/PostRepository.java
@@ -3,11 +3,12 @@ package com.prgrms.board.repository;
 import com.prgrms.board.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = "select p from Post p join fetch p.user", countQuery = "select count(p) from Post p")
+    @EntityGraph(attributePaths = {"user"})
     Page<Post> findAll(Pageable pageable);
+
 }
